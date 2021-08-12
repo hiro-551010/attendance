9fc78fe1f73a7b07b7b782d16bb85689b93f6894
hiro-551010 <hiro551010@gmail.com>
2021-08-12 20:40:17 +0900

    firstcommit

--- payment/forms.py
+++
@@ -1,9 +0,0 @@
-from payment.models import AttendanceEmployee
-from django import forms
-
-
-class AttendanceForm(forms.ModelForm):
-    class Meta:
-        model = AttendanceEmployee
-        fields = ('place', 'in_out')
-
---
+++ slackbot_settings.py
@@ -0,0 +1,4 @@
+
+API_TOKEN = "xoxb-2360616896658-2353878609366-mblaWsG6DK9ZSa9p1bhgBvzK"
+
+DEFAULT_REPLY = "こんにちは、こちらは勤怠管理botです。"
